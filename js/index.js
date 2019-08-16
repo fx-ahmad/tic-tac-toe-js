f0fc8025afbf26ecf2b523c4d9a9e7e8f7048b92
fx-ahmad <fajri.ahmad@lucep.com>
2019-08-16 14:45:18 +0700

    init commit

---
+++ js/index.js
@@ -0,0 +1,82 @@
+var Xpos = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var Opos = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var Xwin = 0;
+var Owin = 0;
+var count = 0;
+var isEnd = false;
+var winCondition = {}
+
+/*---------- Win Condition ----------*/
+winCondition["win_1"] = [1, 1, 1, 0, 0, 0, 0, 0, 0];
+winCondition["win_2"] = [0, 0, 0, 1, 1, 1, 0, 0, 0];
+winCondition["win_3"] = [0, 0, 0, 0, 0, 0, 1, 1, 1];
+winCondition["win_4"] = [1, 0, 0, 1, 0, 0, 1, 0, 0];
+winCondition["win_5"] = [0, 0, 1, 0, 1, 0, 1, 0, 0];
+winCondition["win_6"] = [0, 0, 1, 0, 0, 1, 0, 0, 1];
+winCondition["win_7"] = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+winCondition["win_8"] = [0, 0, 1, 0, 1, 0, 1, 0, 1];
+
+document.getElementById('reset').addEventListener('click', function(){
+    reset();
+    isEnd = false;
+})
+
+var point = document.getElementsByTagName('li');
+for (let i = 0; i < 9; i++) {
+    point[i].addEventListener('click', function () {
+        if (isEnd) {
+            alert('game end, please restart');
+        } else {
+            count++;
+            if (count % 2 == 0) {
+                document.getElementById(point[i].id).className += " disable o btn-primary";
+                document.getElementById(point[i].id).innerHTML = 'O';
+                Opos[point[i].id] = 1;
+                isOwin(Opos);
+            } else {
+                document.getElementById(point[i].id).className += " disable x btn-info";
+                document.getElementById(point[i].id).innerHTML = 'X';
+                Xpos[point[i].id] = 1;
+                isXwin(Xpos);
+            }
+        }
+    })
+}
+
+function isOwin(inGame) {
+    for(let k = 1; k < 9; k++){
+        if(inGame.every((value, index) => value === winCondition["win_"+k][index])){
+            isEnd = true;
+            alert('O win the game');
+            Owin += 1;
+            document.getElementById('o_win').innerHTML = Owin
+        }
+    }
+}
+function isXwin(inGame){
+    for(let x = 1; x < 9; x++){
+        if(inGame.every((value, index) => value === winCondition["win_"+x][index])){
+            isEnd = true;
+            alert('X win the game');
+            Xwin += 1;
+            document.getElementById('x_win').innerHTML = Xwin;
+        }
+    }
+}
+
+function reset() {
+    Opos = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    Xpos = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    count = 0;
+    for (let i = 0; i < 9; i++) {
+        point[i].classList.remove("btn-primary");
+        point[i].classList.remove("btn-info");
+        point[i].classList.remove("o");
+        point[i].classList.remove("x");
+        point[i].classList.remove("disable");
+        point[i].innerHTML = '+';
+    }
+}
+
+
+
